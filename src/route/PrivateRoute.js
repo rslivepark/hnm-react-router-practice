b71f6f5ebc31dd21dc458f6c8cc3c05850ec83e2
rslivepark <rslivepark@gmail.com>
2024-03-28 16:07:26 +0900

    의류 쇼핑몰 만들기2(로그인 기능 추가)

---
+++ src/route/PrivateRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ProductDetail from '../page/ProductDetail';
+import { Navigate } from 'react-router-dom';
+
+export const PrivateRoute = ({ authenticate }) => {
+  return authenticate == true? <ProductDetail></ProductDetail> : <Navigate to='/login'></Navigate>
+};
+
+export default PrivateRoute;
